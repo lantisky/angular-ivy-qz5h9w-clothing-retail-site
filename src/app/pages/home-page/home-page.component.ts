0ce5675f4eb33e10f5765f044784cb6c6f736b07
lantisky <lantislexc@gmail.com>
2021-05-20 13:06:38 +1200

    3 files added

---
+++ src/app/pages/home-page/home-page.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.css']
+})
+export class HomePageComponent implements OnInit {
+  contentLoaded = false;
+  constructor() {}
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.contentLoaded = true;
+    }, 4000);
+  }
+}
