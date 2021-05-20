ea9c6ef09f59e6603bede63ab4b5487954f32e35
lantisky <lantislexc@gmail.com>
2021-05-20 13:21:52 +1200

    4 files modified, 1 added

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ItemComponent } from './pages/item/item.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomePageComponent
+  },
+  {
+    path: 'item',
+    component: ItemComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ItemComponent } from './pages/item/item.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    HomePageComponent,
+    ItemComponent
+  ],
+  imports: [BrowserModule, FormsModule],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
