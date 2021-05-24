ad9b56ba0010640ebf454ff80f5147e04113a33e
lantisky <lantislexc@gmail.com>
2021-05-25 01:39:39 +1200

    5 files modified

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -9,3 +9,21 @@ export class ItemComponent implements OnInit {
   constructor() {}
   ngOnInit() {}
 }
+const data = [
+  {
+    srcUrl: 'https://preview.ibb.co/jrsA6R/img12.jpg',
+    previewUrl: 'https://preview.ibb.co/jrsA6R/img12.jpg'
+  },
+  {
+    srcUrl: 'https://preview.ibb.co/kPE1D6/clouds.jpg',
+    previewUrl: 'https://preview.ibb.co/kPE1D6/clouds.jpg'
+  },
+  {
+    srcUrl: 'https://preview.ibb.co/mwsA6R/img7.jpg',
+    previewUrl: 'https://preview.ibb.co/mwsA6R/img7.jpg'
+  },
+  {
+    srcUrl: 'https://preview.ibb.co/kZGsLm/img8.jpg',
+    previewUrl: 'https://preview.ibb.co/kZGsLm/img8.jpg'
+  }
+];
